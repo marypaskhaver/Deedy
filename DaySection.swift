c5a7c619e8b21ba1d1d4f6318958c0804ab5f52b
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-03-15 16:34:26 -0400

    Add files via upload

---
+++ DaySection.swift
@@ -0,0 +1,28 @@
+//
+//  DaySection.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 3/11/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+
+struct DaySection: TimeSection {
+    var date: Date
+    var deeds: [Deed]
+    
+    static func group(deeds: [Deed]) -> [TimeSection] {
+        let groups = Dictionary(grouping: deeds) { (deed) -> Date in
+            return firstDayOfWeek(date: deed.date)
+        }
+        
+        return groups.map(DaySection.init(date:deeds:))
+    }
+    
+    private static func firstDayOfWeek(date: Date) -> Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: date)
+        return calendar.date(from: components)!
+    }
+}
---
+++ TimeSection.swift
@@ -0,0 +1,16 @@
+//
+//  TimeSection.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 3/15/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+
+protocol TimeSection {
+    var date: Date { get set }
+    var deeds: [Deed] { get set }
+    static func group(deeds : [Deed]) -> [TimeSection]
+//   private static func firstDayOfWeek(date: Date) -> Date
+}
