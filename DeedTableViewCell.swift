b2c6119e7dde6b389f5efb47a695e356fbd968bf
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-03-26 18:19:59 -0400

    Remove excess spaces and comments

--- DeedTableViewCell.swift
+++ DeedTableViewCell.swift
@@ -12,16 +12,12 @@ class DeedTableViewCell: UITableViewCell {
     
     @IBOutlet weak var deedDescriptionLabel: UILabel!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
     }
     
 }
