7104d7924c116c0fa731b537ffba15f265094065
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-03-26 16:46:57 -0400

    Cancel adding deed if its description is whitespace

--- DeedDetailViewController.swift
+++ DeedDetailViewController.swift
@@ -10,19 +10,36 @@ import UIKit
 
 class DeedDetailViewController: UIViewController {
 
-    @IBOutlet weak var deedTF: UITextField!
+    @IBOutlet weak var deedTF: UITextView!
+    
     var deed: Deed!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        deedTF.layer.borderWidth = 1
+        deedTF.layer.cornerRadius = 8
+        deedTF.layer.borderColor = UIColor.gray.cgColor
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "doneSegue" {
+        if segue.identifier == "doneAddingSegue" {
             deed = Deed(withDesc: deedTF.text!)
         }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if (identifier == "doneAddingSegue") {
+            let possibleDeed = Deed(withDesc: deedTF.text!)
+            
+            if (possibleDeed.description.trimmingCharacters(in: .whitespaces).isEmpty)  {
+                self.performSegue(withIdentifier: "cancelAddingSegue", sender: Any?.self)
+                return false
+            }
+        }
+        
+        return true
     }
 
 }
