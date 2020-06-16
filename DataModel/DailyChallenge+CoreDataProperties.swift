c068bbec591c1a98ff224dda4d1dc719273ac891
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-16 12:45:11 -0400

    Add files via upload

---
+++ DataModel/DailyChallenge+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  DailyChallenge+CoreDataProperties.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 6/16/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension DailyChallenge {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<DailyChallenge> {
+        return NSFetchRequest<DailyChallenge>(entityName: "DailyChallenge")
+    }
+
+    @NSManaged public var dailyGoal: Int32
+    @NSManaged public var date: Date
+
+}
