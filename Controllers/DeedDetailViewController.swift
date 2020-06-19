646cdc2aeef0530aa6e3b796d2d6cf8d37a0ff50
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-19 16:56:24 -0400

    Rename deedTF to textView

--- Controllers/DeedDetailViewController.swift
+++ Controllers/DeedDetailViewController.swift
@@ -10,28 +10,29 @@ import UIKit
 
 class DeedDetailViewController: UIViewController {
 
-    @IBOutlet weak var deedTF: UITextView!
-        
+    @IBOutlet weak var textView: UITextView!
+//    weak var delegate: DataEnteredDelegate? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        deedTF.layer.borderWidth = 1
-        deedTF.layer.cornerRadius = 8
-        deedTF.layer.borderColor = UIColor.gray.cgColor
-        
+        textView.layer.borderWidth = 1
+        textView.layer.cornerRadius = 8
+        textView.layer.borderColor = UIColor.gray.cgColor
+
         navigationController?.navigationBar.shadowImage = UIImage()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "doneAddingSegue" {
-            deedTF.text = deedTF.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            textView.text = textView.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         }
     }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         if (identifier == "doneAddingSegue") {
-            if (deedTF.text!.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)  {
+            if (textView.text!.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)  {
                 self.performSegue(withIdentifier: "cancelAddingSegue", sender: Any?.self)
                 return false
             }
