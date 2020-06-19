442833ab30f3d2baad5fdf402fbc0ce2af55fdd6
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-19 09:34:32 -0400

    Call loadColorTheme() upon viewDidLoad so sliders have right values

--- Controllers/SettingsViewController.swift
+++ Controllers/SettingsViewController.swift
@@ -26,6 +26,8 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loadColorTheme()
     }
 
     @IBAction func redSliderChanged(_ sender: UISlider) {
