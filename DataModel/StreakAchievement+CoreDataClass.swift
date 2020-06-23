29e2392b6f60288eff128c652c54afdef3a93e31
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-23 15:23:19 -0400

    Add StreakAchievement CoreData class files

---
+++ DataModel/StreakAchievement+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  StreakAchievement+CoreDataClass.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 6/23/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StreakAchievement)
+public class StreakAchievement: NSManagedObject {
+
+}
