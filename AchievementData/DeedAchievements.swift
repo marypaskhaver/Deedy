ac385cd121f397db83b41cc777e056e7cb3aadf2
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-25 13:03:33 -0400

    Comply with AchievementProtocol

--- AchievementData/DeedAchievements.swift
+++ AchievementData/DeedAchievements.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-class DeedAchievements {
-
-    static let deedAchievements = [
-        ["Complete 5 deeds", 5],
-        ["Complete 10 deeds", 10],
-        ["Complete 25 deeds", 25],
-        ["Complete 50 deeds", 50],
-        ["Complete 75 deeds", 75],
-        ["Complete 100 deeds", 100],
-        ["Complete 200 deeds", 200]
-    ]
+class DeedAchievements: AchievementProtocol {
     
-    static let deedAchievementIdentifier = "deedAchievement"
+    static var achievements: [Dictionary<String, Int>] = [
+        ["Complete 5 deeds" : 5],
+        ["Complete 10 deeds" : 10],
+        ["Complete 25 deeds" : 25],
+        ["Complete 50 deeds" : 50],
+        ["Complete 75 deeds" : 75],
+        ["Complete 100 deeds" : 100],
+        ["Complete 200 deeds" : 200]
+       ]
+    
+    static var identifier: String = "deedAchievement"
     
 }
