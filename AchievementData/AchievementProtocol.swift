e04cf521ba98f15bd079496ae3ad6daf7a85f96a
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-25 13:03:08 -0400

    Add files via upload

---
+++ AchievementData/AchievementProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AchievementProtocol.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 6/25/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+
+protocol AchievementProtocol {
+    static var achievements: [Dictionary<String, Int>] { get set }
+    static var identifier: String { get set }
+}
