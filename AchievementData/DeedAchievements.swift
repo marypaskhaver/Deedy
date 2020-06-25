d8fee75093b1d7c1cd962d870b5b730c1442d6a1
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-25 10:45:21 -0400

    Create DeedAchievements.swift

---
+++ AchievementData/DeedAchievements.swift
@@ -0,0 +1,25 @@
+//
+//  DeedAchievements.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 6/25/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+
+class DeedAchievements {
+
+    static let deedAchievements = [
+        ["Complete 5 deeds", 5],
+        ["Complete 10 deeds", 10],
+        ["Complete 25 deeds", 25],
+        ["Complete 50 deeds", 50],
+        ["Complete 75 deeds", 75],
+        ["Complete 100 deeds", 100],
+        ["Complete 200 deeds", 200]
+    ]
+    
+    static let deedAchievementIdentifier = "deedAchievement"
+    
+}
