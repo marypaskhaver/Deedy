7cef4355bf4fc784df260427956facfdbc38ffe1
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-25 16:15:10 -0400

    Add files via upload

---
+++ Animating/Animations.swift
@@ -0,0 +1,27 @@
+//
+//  Animations.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 6/25/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Animations {
+    
+    static func slideRightToLeftAnimation(duration: TimeInterval, delayFactor: Double) -> Animation {
+        return { cell, indexPath, tableView in
+            cell.transform = CGAffineTransform(translationX: tableView.bounds.width, y: 0)
+
+            UIView.animate(
+                withDuration: 1,
+                delay: 0.1 * Double(indexPath.row),
+                options: [.curveEaseInOut],
+                animations: {
+                    cell.transform = CGAffineTransform(translationX: 0, y: 0)
+            })
+        }
+    }
+}
