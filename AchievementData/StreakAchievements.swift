54763b03cd56f2f04b5d71e54bd70bc64b077906
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-06-25 10:45:48 -0400

    Create StreakAchievements.swift

---
+++ AchievementData/StreakAchievements.swift
@@ -0,0 +1,23 @@
+//
+//  StreakAchievements.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 6/25/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+
+class StreakAchievements {
+    
+    static let streakAchievements = [
+        ["Hit your streak for 1 day", 1],
+        ["Hit your streak for 5 days", 5],
+        ["Hit your streak for 10 days", 10],
+        ["Hit your streak for 15 days", 15],
+        ["Hit your streak for 30 days", 30],
+    ]
+    
+    static let streakAchievementIdentifier = "streakAchievement"
+    
+}
