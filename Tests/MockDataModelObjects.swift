99f23d38d6550464d1f4e6f7fb2b995f42834770
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-05 20:41:53 -0400

    Remove unused code

--- Tests/MockDataModelObjects.swift
+++ Tests/MockDataModelObjects.swift
@@ -35,26 +35,5 @@ class MockDataModelObjects {
         }
         return container
     }()
-//    
-//    func getPersistentContainer() -> NSPersistentContainer {
-//        let container = NSPersistentContainer(name: "DataModel", managedObjectModel: self.managedObjectModel)
-//        
-//        let description = NSPersistentStoreDescription()
-//        description.type = NSInMemoryStoreType
-//        description.shouldAddStoreAsynchronously = false // Make it simpler in test env
-//        
-//        container.persistentStoreDescriptions = [description]
-//        container.loadPersistentStores { (description, error) in
-//            // Check if the data store is in memory
-//            precondition( description.type == NSInMemoryStoreType )
-//                                        
-//            // Check if creating container wrong
-//            if let error = error {
-//                fatalError("Create an in-mem coordinator failed \(error)")
-//            }
-//        }
-//        
-//        return container
-//    }
 
 }
