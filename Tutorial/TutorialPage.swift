b7aa5fd9bc3dd93e357ce2f2006300b4476d21ca
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-12 15:55:24 -0400

    Add files via upload

---
+++ Tutorial/TutorialPage.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialPage.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 7/12/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TutorialPage: UIView {
+    
+    @IBOutlet weak var textView: UITextView!
+
+}
+
