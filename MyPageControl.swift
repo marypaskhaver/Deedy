836ef9e3ebd3f333c7d9ab1b7edb1f783578368d
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-15 14:49:05 -0400

    Add files via upload

---
+++ MyPageControl.swift
@@ -0,0 +1,26 @@
+//
+//  MyPageControl.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 7/15/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import UIKit
+
+class MyPageControl: UIPageControl {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setUp(withScrollView scrollView: TutorialScrollView, inViewController vc: UIViewController) {
+        let numPages = scrollView.createPages(forViewController: vc).count
+
+        self.frame = CGRect(x: scrollView.frame.width / 2, y: scrollView.frame.maxY - 50, width: CGFloat(13 * numPages), height: 37)
+        
+        self.numberOfPages = numPages
+        self.currentPage = 0
+    }
+    
+}
