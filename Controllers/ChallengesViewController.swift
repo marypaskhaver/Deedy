8df3fc92ae9e7a2cfafba136c26d6132e5c714e0
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-15 16:53:13 -0400

    Remove UITableViewDelegate extension to new class

--- Controllers/ChallengesViewController.swift
+++ Controllers/ChallengesViewController.swift
@@ -295,27 +295,3 @@ class ChallengesViewController: UIViewController {
     }
     
 }
-
-// MARK: - TableView Delegate Methods
-extension ChallengesViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
-        header.textLabel?.textAlignment = NSTextAlignment.center
-        header.textLabel?.font = headerFont
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return headerFont.pointSize + 18
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let animation = Animations.slideRightToLeftAnimation(duration: 1, delayFactor: 0.1)
-        let animator = TableViewCellAnimator(animation: animation)
-        animator.animate(cell: cell, at: indexPath, in: tableView)
-    }
-
-}
