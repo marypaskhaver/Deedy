73f5e01bdc57bab3762b9ff2bcd38856e8c2ba53
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-15 16:43:31 -0400

    Move delegate extension in DisplayDeedsViewController here

---
+++ Controllers/DisplayDeedsViewController+Delegate.swift
@@ -0,0 +1,75 @@
+//
+//  DisplayDeedsViewController+Delegate.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 7/15/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension DisplayDeedsViewController: UITableViewDelegate {
+    // Edit deed
+     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+         
+         let editContextItem = UIContextualAction(style: .normal, title: "Edit") {  (contextualAction, view, boolValue) in
+             let storyboard = UIStoryboard(name: "Main", bundle: nil)
+             
+             let evc = storyboard.instantiateViewController(withIdentifier: "EditDeedViewController") as! EditDeedViewController
+             
+             evc.delegate = self
+             
+             evc.oldText = self.dataSource.deeds[indexPath.row].title!
+             
+             self.navigationController?.present(evc, animated: true)
+             
+             self.editedIndexPath = indexPath
+         }
+         
+         // Dark blue
+         editContextItem.backgroundColor = CustomColors.editButtonBlue
+
+         let swipeActions = UISwipeActionsConfiguration(actions: [editContextItem])
+         
+         return swipeActions
+     }
+     
+     func userEditedDeed(newDeedTitle: String) {
+         editedDeedText = newDeedTitle
+         
+         dataSource.deeds[editedIndexPath.row].title = editedDeedText
+
+         editedDeedText = ""
+         editedIndexPath = nil
+         
+         dataSource.saveDeeds()
+         updateSections()
+         
+         tableView.reloadData()
+     }
+     
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         tableView.deselectRow(at: indexPath, animated: true)
+     }
+     
+     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+         if dataSource.sections.isEmpty {
+             return 0
+         }
+         
+         return headerFont.pointSize + 18
+     }
+     
+     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+        header.textLabel?.font = headerFont
+    }
+     
+     // Animate cells here
+     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+         let animation = Animations.slideRightToLeftAnimation(duration: 1, delayFactor: 0.1)
+         let animator = TableViewCellAnimator(animation: animation)
+         animator.animate(cell: cell, at: indexPath, in: tableView)
+     }
+}
