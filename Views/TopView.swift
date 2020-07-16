6eda979c8df5dde50e42a7e3d4c56e45bc2a8120
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-15 22:37:14 -0400

    Rename setHeightInViewController to setFrameInViewController

--- Views/TopView.swift
+++ Views/TopView.swift
@@ -19,12 +19,12 @@ class TopView: UIView {
         self.backgroundColor = (self.traitCollection.userInterfaceStyle == .dark) ? UIColor.black : UIColor.white
     }
     
-    func setHeightInViewController(vc: UIViewController) {
+    func setFrameInViewController(vc: UIViewController) {
         let statusBarHeight = UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarFrame.height
         
         if (vc.navigationController?.navigationBar.frame.height) != nil {
             self.frame = CGRect(x: 0, y: (vc.navigationController?.navigationBar.frame.height)! + (statusBarHeight ?? 0), width: vc.view.frame.width, height: self.frame.height)
         }
     }
-
+    
 }
