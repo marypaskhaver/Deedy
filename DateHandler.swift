30b92feabf9c592d352e4ad175825112ba0955b7
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-16 11:28:47 -0400

    Create DateHandler.swift

---
+++ DateHandler.swift
@@ -0,0 +1,15 @@
+//
+//  DateHandler.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 7/16/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import Foundation
+
+class DateHandler {
+   func currentDate() -> NSDate! {
+      return NSDate()
+   }
+}
