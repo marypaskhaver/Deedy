6479321caa098628acb2eae74b856c288fa1414f
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-16 13:18:16 -0400

    Add files via upload

---
+++ Tests/AddDeedViewControllerTests.swift
@@ -0,0 +1,37 @@
+//
+//  AddDeedViewControllerTests.swift
+//  Good Deed CounterTests
+//
+//  Created by Mary Paskhaver on 7/16/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import XCTest
+
+@testable import Good_Deed_Counter
+
+class AddDeedViewControllerTests: XCTestCase {
+    var advc: AddDeedViewController!
+
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        advc = storyboard.instantiateViewController(identifier: "AddDeedViewController") as? AddDeedViewController
+        advc.loadViewIfNeeded()
+    }
+
+    override func tearDown() {
+        advc = nil
+    }
+    
+    func testInvalidInputWarningLabelHidesWhenViewIsLoaded() {
+        XCTAssertTrue(advc.invalidInputWarningLabel.isHidden)
+    }
+
+    func testInvalidInputWarningLabelShows() {
+        advc.textView.text = "   \n "
+        XCTAssertFalse(advc.shouldPerformSegue(withIdentifier: "doneAddingSegue", sender: UIViewController()))
+        XCTAssertFalse(advc.invalidInputWarningLabel.isHidden)
+    }
+
+}
