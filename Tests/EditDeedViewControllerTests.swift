2a2a41bec1894de5a13fee80361d8ddf0a021d2b
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-16 13:04:05 -0400

    Add files via upload

---
+++ Tests/EditDeedViewControllerTests.swift
@@ -0,0 +1,37 @@
+//
+//  EditDeedViewControllerTests.swift
+//  Good Deed CounterTests
+//
+//  Created by Mary Paskhaver on 7/16/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+import XCTest
+
+@testable import Good_Deed_Counter
+
+class EditDeedViewControllerTests: XCTestCase {
+    var edvc: EditDeedViewController!
+
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        edvc = storyboard.instantiateViewController(identifier: "EditDeedViewController") as? EditDeedViewController
+        edvc.loadViewIfNeeded()
+    }
+
+    override func tearDown() {
+        edvc = nil
+    }
+    
+    func testInvalidInputWarningLabelHidesWhenViewIsLoaded() {
+        XCTAssertTrue(edvc.invalidInputWarningLabel.isHidden)
+    }
+
+    func testInvalidInputWarningLabelShows() {
+        edvc.textView.text = "   \n "
+        edvc.doneButtonPressed(UIBarButtonItem())
+        XCTAssertFalse(edvc.invalidInputWarningLabel.isHidden)
+    }
+
+}
