7725d915486c45e04e76d527e64d5aca510cd83e
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-19 11:03:12 -0400

    Add files via upload

---
+++ DataModel/UserDefaultsKeys.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKeys.swift
+//  Good Deed Counter
+//
+//  Created by Mary Paskhaver on 7/19/20.
+//  Copyright © 2020 Nostaw. All rights reserved.
+//
+
+class UserDefaultsKeys {
+    static let navBarColor = "navBarColor"
+    static let navBarTextColor = "navBarTextColor"
+}
