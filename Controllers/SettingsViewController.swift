3bbc6fe884f827124bb3e2ffc6b1c2b72f3339a0
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-19 11:28:57 -0400

    Move UserDefaults color extension to separate class

--- Controllers/SettingsViewController.swift
+++ Controllers/SettingsViewController.swift
@@ -167,29 +167,3 @@ class SettingsViewController: UIViewController {
         navigationController?.navigationBar.barTintColor = getUIColorFromSliders()
     }
 }
-
-// MARK: - UserDefaults methods
-extension UserDefaults {
-    func color(forKey key: String) -> UIColor? {
-        guard let colorData = data(forKey: key) else { return nil }
-
-        do {
-            return try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: colorData)
-        } catch let error {
-            print("Color error \(error)")
-            return nil
-        }
-    }
-
-    func set(_ value: UIColor?, forKey key: String) {
-        guard let color = value else { return }
-        
-        do {
-            let data = try NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: false)
-            set(data, forKey: key)
-        } catch let error {
-            print("Error color key data not saved \(error)")
-        }
-    }
-
-}
