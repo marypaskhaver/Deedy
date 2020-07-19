0162d7be52324b84aff0b763d35b6b85fb0874c5
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-07-19 11:08:17 -0400

    Add keys for SettingsVC sliders and showing tutorials

--- DataModel/UserDefaultsKeys.swift
+++ DataModel/UserDefaultsKeys.swift
@@ -9,4 +9,12 @@
 class UserDefaultsKeys {
     static let navBarColor = "navBarColor"
     static let navBarTextColor = "navBarTextColor"
+    
+    static let redSliderValue = "redSliderValue"
+    static let greenSliderValue = "greenSliderValue"
+    static let blueSliderValue = "blueSliderValue"
+    
+    static let displayDeedsViewControllerTutorialShown = "DisplayDeedsViewControllerTutorialShown"
+    static let challengesViewControllerTutorialShown = "ChallengesViewControllerTutorialShown"
+    static let settingsViewControllerTutorialShown = "SettingsViewControllerTutorialShown"
 }
