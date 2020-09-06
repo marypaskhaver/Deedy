14ec987e24366f9dd317c07a78f6ae3c2310f3d3
marypaskhaver <45778203+marypaskhaver@users.noreply.github.com>
2020-09-05 22:18:37 -0400

    Revert resetAction's options to .init(rawValue: 0)

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -67,7 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     }
     
     func configureCategory(forCenter center: UNUserNotificationCenter) {
-        let resetAction = UNNotificationAction(identifier: resetQuoteActionIdentifier, title: "Get a new quote!", options: .foreground)
+        let resetAction = UNNotificationAction(identifier: resetQuoteActionIdentifier, title: "Get a new quote!", options: .init(rawValue: 0))
         
         let category = UNNotificationCategory(identifier: categoryIdentifier, actions: [resetAction], intentIdentifiers: [], options: [])
         
