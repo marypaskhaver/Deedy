42fc2f82c2e53f945157a631219943f1fdb9979e
marypaskhaver <mary.paskhaver@gmail.com>
2021-11-28 17:39:56 -0500

    Rewrite setBorderColor to set to a given color

--- Good Deed Counter/ColoredBorderButton.swift
+++ Good Deed Counter/ColoredBorderButton.swift
@@ -17,7 +17,7 @@ class ColoredBorderButton: UIButton {
         self.layer.cornerRadius = 30
         self.layer.borderWidth = 2
         
-        setBorderColor()
+//        setBorderColor()
 
         // Constrain width and height based on current screen width.
         self.addConstraint(NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 0.56 * UIScreen.main.bounds.width))
@@ -26,11 +26,7 @@ class ColoredBorderButton: UIButton {
     }
     
     // If the app's nav bar was set to a custom color, set borderColor to nav bar's color. Otherwise, set border color to defaultBlue color.
-    func setBorderColor() {
-        if let navBarColor = defaults.color(forKey: UserDefaultsKeys.navBarColor) {
-            self.layer.borderColor = navBarColor.cgColor
-        } else {
-            self.layer.borderColor = CustomColors.defaultBlue.cgColor
-        }
+    func setBorderColor(to color: CGColor) {
+        self.layer.borderColor = color
     }
 }
