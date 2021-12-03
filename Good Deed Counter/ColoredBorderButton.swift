c5c43d041b838f6f9ae27e5862301e7a7ca74bf5
marypaskhaver <mary.paskhaver@gmail.com>
2021-12-02 22:32:49 -0500

    Change height constraint, remove comments

--- Good Deed Counter/ColoredBorderButton.swift
+++ Good Deed Counter/ColoredBorderButton.swift
@@ -17,15 +17,12 @@ class ColoredBorderButton: UIButton {
         self.layer.cornerRadius = 30
         self.layer.borderWidth = 2
         
-//        setBorderColor()
-
         // Constrain width and height based on current screen width.
         self.addConstraint(NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 0.56 * UIScreen.main.bounds.width))
         
-        self.addConstraint(NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.frame.width / 12))
+        self.addConstraint(NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.frame.width / 20))
     }
     
-    // If the app's nav bar was set to a custom color, set borderColor to nav bar's color. Otherwise, set border color to defaultBlue color.
     func setBorderColor(to color: CGColor) {
         self.layer.borderColor = color
     }
